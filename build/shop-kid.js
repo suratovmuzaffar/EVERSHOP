18158d462753efffd3f0148cf7c53a6504147f80
muhammadamin7007 <muhammadaminabdugafforov109@gmail.com>
2025-03-24 10:06:43 +0500

    shop kids color and size filter full

--- build/shop-kid.js
+++ build/shop-kid.js
@@ -5,11 +5,13 @@ document.addEventListener("DOMContentLoaded", () => {
   const m = document.getElementById("m");
   const l = document.getElementById("l");
   const x = document.getElementById("x");
+  const white = document.getElementById("color-white");
+  const black = document.getElementById("color-black");
+  const pink = document.getElementById("color-pink");
+  const red = document.getElementById("color-red");
+  const purple = document.getElementById("color-purple");
   const container = document.getElementById("list-product");
   const defaultHTML = container.innerHTML;
-  if (xl.checked) {
-    renderProducts(xlproduct);
-  }
   xl.addEventListener("change", () => {
     if (xl.checked) {
       renderProducts(xlproduct);
@@ -17,9 +19,6 @@ document.addEventListener("DOMContentLoaded", () => {
       container.innerHTML = defaultHTML;
     }
   });
-  if (l.checked) {
-    renderProducts(lproduct);
-  }
   l.addEventListener("change", () => {
     if (l.checked) {
       renderProducts(lproduct);
@@ -27,9 +26,6 @@ document.addEventListener("DOMContentLoaded", () => {
       container.innerHTML = defaultHTML;
     }
   });
-  if (m.checked) {
-    renderProducts(mproduct);
-  }
   m.addEventListener("change", () => {
     if (m.checked) {
       renderProducts(mproduct);
@@ -37,9 +33,6 @@ document.addEventListener("DOMContentLoaded", () => {
       container.innerHTML = defaultHTML;
     }
   });
-  if (x.checked) {
-    renderProducts(xproduct);
-  }
   x.addEventListener("change", () => {
     if (x.checked) {
       renderProducts(xproduct);
@@ -47,12 +40,44 @@ document.addEventListener("DOMContentLoaded", () => {
       container.innerHTML = defaultHTML;
     }
   });
-  if (s.checked) {
-    renderProducts(sproduct);
-  }
   s.addEventListener("change", () => {
     if (s.checked) {
       renderProducts(sproduct);
+    } else {
+      container.innerHTML = defaultHTML;
+    }
+  });
+  white.addEventListener("change", () => {
+    if (white.checked) {
+      renderProducts(whiteProducts);
+    } else {
+      container.innerHTML = defaultHTML;
+    }
+  });
+  black.addEventListener("change", () => {
+    if (black.checked) {
+      renderProducts(blackProducts);
+    } else {
+      container.innerHTML = defaultHTML;
+    }
+  });
+  pink.addEventListener("change", () => {
+    if (pink.checked) {
+      renderProducts(pinkProducts);
+    } else {
+      container.innerHTML = defaultHTML;
+    }
+  });
+  red.addEventListener("change", () => {
+    if (red.checked) {
+      renderProducts(redProducts);
+    } else {
+      container.innerHTML = defaultHTML;
+    }
+  });
+  purple.addEventListener("change", () => {
+    if (purple.checked) {
+      renderProducts(purpleProducts);
     } else {
       container.innerHTML = defaultHTML;
     }
@@ -63,15 +88,13 @@ var xlproduct = [
     id: 1,
     name: "Continental 80 shoes",
     price: 126,
-    image: "./img/xl.png",
-    link: "/kids/continental-80-shoes"
+    image: "./img/xl.png"
   },
   {
     id: 2,
     name: "Swift run x shoes",
     price: 337,
-    image: "https://demositefiles.blob.core.windows.net/images/catalog/9804/3444/plv1919-Purple-listing.png",
-    link: "/kids/swift-run-x-shoes"
+    image: "https://demositefiles.blob.core.windows.net/images/catalog/9804/3444/plv1919-Purple-listing.png"
   }
 ];
 var lproduct = [
@@ -79,15 +102,13 @@ var lproduct = [
     id: 1,
     name: "Continental 80 shoes",
     price: 126,
-    image: "./img/nike-002.png",
-    link: "/kids/continental-80-shoes"
+    image: "./img/nike-002.png"
   },
   {
     id: 2,
     name: "Swift run x shoes",
     price: 337,
-    image: "https://demositefiles.blob.core.windows.net/images/catalog/9804/3444/plv1919-Purple-listing.png",
-    link: "/kids/swift-run-x-shoes"
+    image: "https://demositefiles.blob.core.windows.net/images/catalog/9804/3444/plv1919-Purple-listing.png"
   }
 ];
 var mproduct = [
@@ -95,8 +116,7 @@ var mproduct = [
     id: 1,
     name: "Continental 80 shoes",
     price: 126,
-    image: "./img/nike-003.png",
-    link: "/kids/continental-80-shoes"
+    image: "./img/nike-003.png"
   }
 ];
 var xproduct = [
@@ -104,15 +124,13 @@ var xproduct = [
     id: 1,
     name: "Continental 80 shoes",
     price: 126,
-    image: "./img/xl.png",
-    link: "/kids/continental-80-shoes"
+    image: "./img/xl.png"
   },
   {
     id: 2,
     name: "Swift run x shoes",
     price: 337,
-    image: "./img/nike-005.png",
-    link: "/kids/swift-run-x-shoes"
+    image: "./img/nike-005.png"
   }
 ];
 var sproduct = [
@@ -120,8 +138,59 @@ var sproduct = [
     id: 1,
     name: "Continental 80 shoes",
     price: 126,
-    image: "https://demositefiles.blob.core.windows.net/images/catalog/1840/2713/plv2390-Red-listing.png",
-    link: "/kids/continental-80-shoes"
+    image: "https://demositefiles.blob.core.windows.net/images/catalog/1840/2713/plv2390-Red-listing.png"
+  }
+];
+var whiteProducts = [
+  {
+    id: 1,
+    name: "White Sneakers",
+    price: 120,
+    image: "./img/xl.png"
+  },
+  {
+    id: 2,
+    name: "Taylor 002",
+    price: 620,
+    image: "./img/nike-005.png"
+  }
+];
+var blackProducts = [
+  {
+    id: 1,
+    name: "Black Running Shoes",
+    price: 350,
+    image: "./img/black.png"
+  },
+  {
+    id: 2,
+    name: "Black Running Shoes",
+    price: 1250,
+    image: "./img/black-001.png"
+  }
+];
+var pinkProducts = [
+  {
+    id: 3,
+    name: "Pink Shoes",
+    price: 135,
+    image: "./img/nike-002.png"
+  }
+];
+var redProducts = [
+  {
+    id: 11,
+    name: "Red Sneakers",
+    price: 140,
+    image: " https://demositefiles.blob.core.windows.net/images/catalog/1840/2713/plv2390-Red-listing.png"
+  }
+];
+var purpleProducts = [
+  {
+    id: 4,
+    name: "Red Sneakers",
+    price: 140,
+    image: "https://demositefiles.blob.core.windows.net/images/catalog/9804/3444/plv1919-Purple-listing.png"
   }
 ];
 function renderProducts(products) {
@@ -147,114 +216,3 @@ function renderProducts(products) {
   }
   container.replaceChildren(fragment);
 }
-document.addEventListener("DOMContentLoaded", () => {
-  const white = document.getElementById("white");
-  const black = document.getElementById("black");
-  const pink = document.getElementById("pink");
-  const red = document.getElementById("red");
-  const purple = document.getElementById("purple");
-  const container = document.getElementById("list-product");
-  const defaultHTML = container.innerHTML;
-  if (white.checked) {
-    renderProducts(whiteProducts);
-  }
-  white.addEventListener("change", () => {
-    if (white.checked) {
-      renderProducts(whiteProducts);
-    } else {
-      container.innerHTML = defaultHTML;
-    }
-  });
-  if (black.checked) {
-    renderProducts(blackProducts);
-  }
-  black.addEventListener("change", () => {
-    if (black.checked) {
-      renderProducts(blackProducts);
-    } else {
-      container.innerHTML = defaultHTML;
-    }
-  });
-  if (pink.checked) {
-    renderProducts(pinkProducts);
-  }
-  pink.addEventListener("change", () => {
-    if (pink.checked) {
-      renderProducts(pinkProducts);
-    } else {
-      container.innerHTML = defaultHTML;
-    }
-  });
-  if (red.checked) {
-    renderProducts(redProducts);
-  }
-  red.addEventListener("change", () => {
-    if (red.checked) {
-      renderProducts(redProducts);
-    } else {
-      container.innerHTML = defaultHTML;
-    }
-  });
-  if (purple.checked) {
-    renderProducts(purpleProducts);
-  }
-  purple.addEventListener("change", () => {
-    if (purple.checked) {
-      renderProducts(purpleProducts);
-    } else {
-      container.innerHTML = defaultHTML;
-    }
-  });
-});
-var whiteProducts = [
-  {
-    id: 1,
-    name: "White Sneakers",
-    price: 120,
-    image: "./img/xl.png",
-    link: "/kids/white-sneakers"
-  },
-  {
-    id: 2,
-    name: "Taylor 002",
-    price: 620,
-    image: "./img/nike-005.png",
-    link: "/kids/white-sneakers"
-  }
-];
-var blackProducts = [
-  {
-    id: 2,
-    name: "Black Running Shoes",
-    price: 150,
-    image: "./img/black.png",
-    link: "/kids/black-running-shoes"
-  }
-];
-var pinkProducts = [
-  {
-    id: 3,
-    name: "Pink Shoes",
-    price: 135,
-    image: "./img/nike-002.png",
-    link: "/kids/pink-shoes"
-  }
-];
-var redProducts = [
-  {
-    id: 4,
-    name: "Red Sneakers",
-    price: 140,
-    image: "./img/red.png",
-    link: "/kids/red-sneakers"
-  }
-];
-var purpleProducts = [
-  {
-    id: 5,
-    name: "Purple Shoes",
-    price: 145,
-    image: "./img/purple.png",
-    link: "/kids/purple-shoes"
-  }
-];
